17bee2c2209eef5b32d118758c4ac3ff3640e424
Krus <krus.margo@gmail.com>
2025-06-30 22:56:01 +0300

    try merge

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,35 +1,48 @@
-import { model, Schema } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: false,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    }, 
+    fullName: { type: String, 
+        required: false },
+    avatarUrl: {
+      type: String,
+      default: '',
     },
-    favoriteRecipes: {
-        type: [Schema.Types.ObjectId],
-        ref: "recipe",
-        default: [],
-    }
-},
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+    
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
+//cховаємо пароль для фронтенду
 userSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
 };
 
-export const UserCollection = model("user", userSchema);
+export const UserCollection = model('user', userSchema);
--- src/server.js
+++ src/server.js
@@ -1,42 +1,43 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import pino from "pino-http";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import pino from 'pino-http';
+import cors from 'cors';
 
 import userRoutes from './routers/user.js';
-import { errorHandler } from "./middlewares/errorHandler.js";
-import { notFoundHandler } from "./middlewares/notFoundHandler.js";
-import { getEnvVar } from "./utils/getEnvVar.js";
-import allRouters from "./routers/index.js";
-
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import allRouters from './routers/index.js';
 
 export const setupServer = () => {
-    dotenv.config();
+  dotenv.config();
 
-    const PORT = getEnvVar("PORT", "3000");
-    const app = express();
-    
-    app.use('/api/user', userRoutes);
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use(pino({
-        transport: {
-            target: 'pino-pretty',
-        },
-    }),);
-    app.use(cors());
-    app.use("/api", allRouters);
-    // app.use('/uploads', express.static(UPLOAD_DIR));
-    // app.use('/api-docs', swaggerDocs());
+  const PORT = getEnvVar('PORT', '3000');
+  const app = express();
 
-    app.use(notFoundHandler);
-    app.use(errorHandler);
-    app.listen(PORT, (error) => {
-        if (error) {
-            throw error;
-        }
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.use(express.json());
+  app.use(cookieParser());
+    app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
 
+  app.use('/api/user', userRoutes);
+  app.use('/api', allRouters);
+  // app.use('/uploads', express.static(UPLOAD_DIR));
+  // app.use('/api-docs', swaggerDocs());
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+  app.listen(PORT, (error) => {
+    if (error) {
+      throw error;
+    }
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
